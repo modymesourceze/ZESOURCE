5a21dc87f7fc9dd1258e7172a3680bcda4fd0a20
modymesourceze <148470115+modymesourceze@users.noreply.github.com>
2023-12-15 16:19:49 +0200

    Add files via upload

--- SourceZe/assistant/start.py
+++ SourceZe/assistant/start.py
@@ -37,7 +37,7 @@ async def start(event):
             buttons=[
                                      [Button.inline("عرض المستخدمين 📬", data="users"), Button.inline(
                                          "اوامر البـوت ⚒️", data="gibcmd")],
-                                     [Button.url("المطـور 🔗", "https://t.me/UP_UO"), Button.inline(
+                                     [Button.url("المطـور 🔗", "https://t.me/ELHYBA"), Button.inline(
                                          "اوامر الزغـرفة", data="rozzag")],
 
                                  ])
@@ -52,7 +52,7 @@ async def start(event):
             link_preview=False,
             buttons=[
                 [custom.Button.inline("تنـصيب زد إي  🐍", data="deploy")],
-                [Button.url("تحتاج مسـاعدة ❓", "https://t.me/UP_UO")],
+                [Button.url("تحتاج مسـاعدة ❓", "https://t.me/ELHYBA")],
             ],
         )
 
@@ -66,8 +66,8 @@ async def help(event):
             event.chat_id,
             message="**لتـنصيب البـوت الخاص بك اتبـع الخطـوات في الاسفـل وحاول واذا لم تستطيع تفضل الى مجموعة المساعدة ليساعدوك 🧸♥**.",
             buttons=[
-                [Button.url("شرح التنصيب 📺", "https://t.me/UI_XB")],
-                [Button.url("كروب المساعدة ❓", "https://t.me/UI_OS")],
+                [Button.url("شرح التنصيب 📺", "https://t.me/Source_Ze")],
+                [Button.url("كروب المساعدة ❓", "https://t.me/ZeSupport")],
             ],
         )
 
@@ -107,7 +107,7 @@ async def starkislub(event):
 
 @tgbot.on(events.NewMessage(pattern="^/alive", func=lambda e: e.sender_id == bot.uid))
 async def starkislub(event):
-    razan = "**𝙕𝞝 𝐔𝐒𝐄𝐑𝐁𝐎𝐓**\n•━═━═━═━═━━═━═━═━═━•‌‌\n**- حالة البوت **  يعمـل بنجـاح\n**- اصدار التليثون  **: 1.23.0\n**- اصدار البايثون **: 3.9.6\n**- يوزرك ** {mention}\n**- CH : @UI_XB\n•━═━═━═━═━━═━═━═━═━•‌‌\n"
+    razan = "**𝙕𝞝 𝐔𝐒𝐄𝐑𝐁𝐎𝐓**\n•━═━═━═━═━━═━═━═━═━•‌‌\n**- حالة البوت **  يعمـل بنجـاح\n**- اصدار التليثون  **: 1.23.0\n**- اصدار البايثون **: 3.9.6\n**- يوزرك ** {mention}\n**- CH : @Source_Ze\n•━═━═━═━═━━═━═━═━═━•‌‌\n"
     await event.reply(razan)
     
     
@@ -134,7 +134,7 @@ async def settings(event):
                                      "اسماء القنوات َِ🛹", data="chanlan")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @UI_XB", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Source_Ze", alert=True)
 
 
 
@@ -154,7 +154,7 @@ async def settings(event):  #    قـسـم  الزغرفـة جمـثـون
                                          "║ رجوع ║ ⁦⁩", data="rozzag")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @UI_XB", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Source_Ze", alert=True)
 
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"razan")))  
@@ -172,7 +172,7 @@ async def settings(event):  #    قـسـم  الزغرفـة لأسـماء ا�
                                          "║ رجوع ║", data="rozname")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @UI_XB", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Source_Ze", alert=True)
 
 
 
@@ -217,7 +217,7 @@ async def settings(event): #    قـسـم  الزغرفـة لأسـماء ال
                                          "║ رجوع ║", data="rozname")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @UI_XB", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Source_Ze", alert=True)
 
 # شنو تـدور  :)
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"RR7PP1")))
@@ -258,7 +258,7 @@ async def settings(event):  #    قـسـم  البـايو 1
                                          "⫷ التالي ", data="rozpio2")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @UI_XB", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Source_Ze", alert=True)
 
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"rozpio2"))) 
@@ -276,7 +276,7 @@ async def settings(event): #    قـسـم  البـايو 2
                                          "⫷ التالي", data="rozpio3")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @UI_XB", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Source_Ze", alert=True)
 
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"rozpio3"))) 
@@ -294,7 +294,7 @@ async def settings(event): #    قـسـم  البـايو 3
                                          "⫷ التالي", data="rozpio4")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @UI_XB", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Source_Ze", alert=True)
 
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"rozpio4"))) 
@@ -312,7 +312,7 @@ async def settings(event): #    قـسـم  البـايو 4
                                          "⫷ التالي", data="rozpio5")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @UI_XB", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Source_Ze", alert=True)
 
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"rozpio5"))) 
@@ -330,7 +330,7 @@ async def settings(event):#    قـسـم  البـايو 5
                                          "⫷ التالي", data="rozpio1")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @UI_XB", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Source_Ze", alert=True)
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"rozmonth")))  
 async def settings(event): #    قـسم الـمواليـد و الأشـهر
@@ -347,7 +347,7 @@ async def settings(event): #    قـسم الـمواليـد و الأشـهر
                                          "║ رجوع ║", data="rozzag")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @UI_XB", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Source_Ze", alert=True)
 
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"months")))  
