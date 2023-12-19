82482f484e21fa812bae97b61c6b4fa953a060fa
modymesourceze <148470115+modymesourceze@users.noreply.github.com>
2023-12-19 12:01:36 +0200

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -29,7 +29,7 @@ class Config(object):
     TZ = os.environ.get("TZ", "Asia/Baghdad")
     # set this with required cat repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/Source-Ze/ZESOURCE.git"
+        "UPSTREAM_REPO", "https://github.com/modymesourceze/ZESOURCE.git"
     )
 
     # BASIC and MAIN CONFIG VARS
