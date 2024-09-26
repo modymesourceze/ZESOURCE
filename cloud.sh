4b88713bcd199991862d4ac6bc425fb69dba1107
Mody <148470115+modymesourceze@users.noreply.github.com>
2024-09-26 14:42:49 +0300

    Add files via upload

---
+++ cloud.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cat <<'EOF'
+$$$$$$$$\    $$$$$\ $$\                           
+\____$$  |   $$  __|$$|                          
+    $$  /    $$ |______
+   $$  /     $$$$$\ $$\
+  $$  /      $$  _____|
+ $$  /       $$ |
+ $$$$$$$$\   $$$$$\$$$| 
+\________|   \________/
+                                                  
+                                                  
+                                                  
+Copyright (C) 2020-2024 by modymesourceze@Github, < https://github.com/modymesourceze >.
+This file is part of < https://github.com/modymesourceze/ZESOURCE > project,
+and is released under the "GNU v3.0 License Agreement".
+Please see < https://github.com/modymesourceze/ZESOURCE/blob/main/LICENSE >
+All rights reserved.
+EOF
+
+gunicorn app:app --daemon && python -m SourceZe
